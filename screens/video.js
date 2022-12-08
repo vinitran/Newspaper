d13482bc51d9750eba7bab341c3543062979d5b3
cheSau <hongquanvd1@gmail.com>
2022-12-08 22:18:21 +0700

    use axios to fetch data

--- screens/video.js
+++ screens/video.js
@@ -1,15 +1,28 @@
 import { StyleSheet } from 'react-native';
 import * as React from 'react';
 import Screen from '../components/Screen';
-import data from "../fakeDataVideo"
 import VideoNews from '../components/VideoNews';
+import axios from 'axios';
 
 
 const ITEM_HEIGHT = 350;
 export default function VideoScreen() {
     const [focusedIndex, setFocusedIndex] = React.useState(0);
 
-    const videoNews = data;
+    const [videoNews, setVideoNews] = React.useState([]);
+    React.useEffect(() => {
+        async function fetchVideos() {
+            try {
+                const requestUrl = "";
+                const data = await axios.get(requestUrl)
+                    .then(res => res.data)
+                setVideoNews(data)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchVideos()
+    }, [])
     const handleScroll = React.useCallback((e) => {
         const offset = Math.round(e.nativeEvent.contentOffset.y / ITEM_HEIGHT);
         setFocusedIndex(offset)
