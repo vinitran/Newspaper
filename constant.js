dd3f65cf6bca4f67aee5d40d99fff0eb5e36ea30
cheSau <hongquanvd1@gmail.com>
2022-12-09 23:02:19 +0700

    change video screen llike tiktok

---
+++ constant.js
@@ -0,0 +1,3 @@
+import { Dimensions } from "react-native";
+
+export const { width: windowWidth, height: windowHeight } = Dimensions.get("window")
--- screens/video.js
+++ screens/video.js
@@ -1,36 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import * as React from 'react';
-import Screen from '../components/Screen';
-import VideoNews from '../components/VideoNews';
-import axios from 'axios';
+import VideoItem from '../components/VideoItem';
+import video from "../fakeDataVideo"
+import { windowHeight } from '../constant';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 
-
-const ITEM_HEIGHT = 350;
 export default function VideoScreen() {
-    const [focusedIndex, setFocusedIndex] = React.useState(0);
-
-    const [videoNews, setVideoNews] = React.useState([]);
-    React.useEffect(() => {
-        async function fetchVideos() {
-            try {
-                const requestUrl = "";
-                const data = await axios.get(requestUrl)
-                    .then(res => res.data)
-                setVideoNews(data)
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchVideos()
-    }, [])
-    const handleScroll = React.useCallback((e) => {
-        const offset = Math.round(e.nativeEvent.contentOffset.y / ITEM_HEIGHT);
-        setFocusedIndex(offset)
-    }, [setFocusedIndex]);
+    const data = video;
+    const [activeVideoIndex, setActiveVideoIndex] = React.useState(0)
+    const bottomTabHeight = useBottomTabBarHeight();
     return (
-        <Screen handleScroll={handleScroll} >
-            <VideoNews data={videoNews} focusedIndex={focusedIndex} />
-        </Screen >
+        <FlatList
+            data={data}
+            pagingEnabled
+            renderItem={({ item, index }) => <VideoItem data={item} isActive={activeVideoIndex === index} />}
+            onScroll={e => {
+                const index = Math.round(e.nativeEvent.contentOffset.y / (windowHeight - bottomTabHeight))
+                setActiveVideoIndex(index)
+            }}
+        />
+
+
+
     );
 }
 
