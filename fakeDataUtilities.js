e371d43b2930ff0fcdeab81cc37a17d5a16daa77
Quốc Toàn <79847902+toancris37@users.noreply.github.com>
2022-12-11 08:14:39 +0700

    Update fakeDataUtilities.js

--- fakeDataUtilities.js
+++ fakeDataUtilities.js
@@ -1,13 +1,29 @@
 export default [
     {
         id: '1',
-        imageUrl: 'https://picsum.photos/seed/picsum/300/200',
-        title: "Messi 2 - 0 Mexico GG EZ WP Nb",
+        imageUrl: 'https://github.com/vinitran/Newspaper/blob/main/assets/images/calendar.png?raw=true',
+        title: "Lịch",
+        height: '200'
     },
     {
         id: '2',
-        imageUrl: 'https://picsum.photos/seed/picsum/300/200',
-        title: "Messi 2 - 0 Mexico GG EZ WP Nb",
+        imageUrl: 'https://github.com/vinitran/Newspaper/blob/main/assets/images/xoso.png?raw=true',
+        title: "Xổ số",
+        height: '100'
+
+    },
+    {
+        id: '3',
+        imageUrl: 'https://github.com/vinitran/Newspaper/blob/main/assets/images/football.png?raw=true',
+        title: "Bóng đá",
+        height: '150'
+
+    },
+    {
+        id: '4',
+        imageUrl: 'https://github.com/vinitran/Newspaper/blob/main/assets/images/weather.png?raw=true',
+        title: "Thời tiết",
+        height: '250'
     },
 
 ]
